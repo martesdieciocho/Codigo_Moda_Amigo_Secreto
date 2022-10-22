1703c238c8caf78e1ba5066e29f5216a32986b19
martesdieciocho <alejo.9418@hotmail.com>
2022-10-22 12:32:53 -0500

    Creación de proyecto, instalación de paquetes y creación de modelo user para la bd

---
+++ database/models/users.js
@@ -0,0 +1,63 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "User"
+
+    let cols ={
+        id:{
+            type: dataTypes.INTEGER,
+            autoIncremente: true,
+            primaryKey: true
+        },
+        first_name:{
+            type: dataTypes.STRING,
+            
+        },
+        last_name:{
+            type: dataTypes.STRING
+        },
+        pleasures:{
+            type: dataTypes.STRING
+        }
+        ,
+        dislikes:{
+            type: dataTypes.STRING
+        },
+        allergies:{
+            type: dataTypes.STRING
+        },
+        rol_id:{
+            type: person_id.INTEGER
+        },
+        person_id:{
+            type: dataTypes.INTEGER
+        },
+        community_id:{
+            type: dataTypes.INTEGER
+        }
+
+    }
+
+    let config={
+        tableName: "user",
+        timestamps: false
+    }
+
+    let User =sequelize.define(alias,cols,config)
+
+    User.associate = models => {
+        User.belongsTo(models.Person,{
+            as: "person",
+            foreingKey: "person_id"
+        })
+    }
+
+    User.associate = models => {
+        User.belongsTo(models.Community,{
+            as: "community",
+            foreingKey: "community_id"
+        })
+    }
+
+    
+    
+    return User;
+}
