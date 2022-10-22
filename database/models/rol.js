873b82d008372476f9e88e8a56093d0a1b6af3af
Camilo1002 <camilootfm@gmail.com>
2022-10-22 14:39:23 -0500

    Creacion de modelos rol y user_rol

---
+++ database/models/rol.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Rol"
+
+    let cols ={
+        id:{
+            type: dataTypes.INTEGER,
+            autoIncremente: true,
+            primaryKey: true
+        },
+
+        name:{
+            type: dataTypes.STRING
+        }
+
+    }
+      let config={
+        tableName: "rol",
+        timestamps: false
+    }
+
+    let Rol =sequelize.define(alias,cols,config)
+
+    Rol.associate = models => {
+        Rol.hasMany(models.User_rol,{
+            as: "User_rol",
+            foreingKey: "id"
+        })
+    }
+    
+    return Rol;
+}
---
+++ database/models/user_rol.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "User_rol"
+
+    let cols ={
+        user_id:{
+            type: dataTypes.INTEGER,
+            
+        },
+
+        rol_id:{
+            type: dataTypes.INTEGER,
+            
+        }
+    }   
+        let config={
+            tableName: "user_rol",
+            timestamps: false
+        }
+
+        let User_rol =sequelize.define(alias,cols,config)
+    
+        User_rol.associate = models => {
+            User_rol.hasMany(models.Rol,{
+                as: "rols",
+                through: "user_rol",
+                foreingKey: "rol_id",
+                otherKey: "id",
+                timestamps: false
+            })
+    }
+        User_rol.associate = models => {
+            User_rol.hasMany(models.Users,{
+            as: "users",
+            through: "user_rol",
+            foreingKey: "user_id",
+            otherKey: "id",
+            timestamps: false
+    })
+}
+          
+}
